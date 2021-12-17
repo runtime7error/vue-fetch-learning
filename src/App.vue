af88079b16b791b59cf44e1ad80e567f2f6598fb
runtime7error <lucaslh6923@gmail.com>
2021-12-16 21:32:16 -0300

    change background and component colors; add shadow to main img; taillwind update; btn color changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="bg-pink-400 h-screen">
+  <div class="bg-pink  h-screen">
+    <!-- Title -->
     <div>
-      <h1 class="text-4xl text-white p-6">Anime Photo Randomizer</h1>
+      <h1 class="text-4xl outline outline-offset-2 outline-4 text-font p-6 ">Anime Photo Randomizer</h1>
     </div>
+
+    <!-- Anime Photo -->
     <div class="grid place-items-center w-2/4 h-2/4">
-      <div class=" w-64 h-64 grid place-items-center">
-        <img style="width: 100%; height: 100%;" class="border-4 border-black" :src=waifuImg>
+      <div class=" w-64 h-64 grid place-items-center mb-28">
+        <img style="width: 100%; height: 100%; box-shadow: 0 0 0.6em #a3aaaf;" class="border-4 border-button-hover rounded-md shadow-md"  :src=waifuImg>
       </div>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-52" @click="randomize">
+
+      <!-- Option Menu -->
+      <div>
+        <select v-model="type" class="w-full h-full border-2 border-button-hover text-center mt-5 mb-5 rounded-md">
+          <option value="waifu">Waifu</option>
+          <option value="neko">Neko</option>
+          <option value="megumin">Megumin</option>
+          <option value="hug">Hug</option>
+        </select>
+      </div>
+      <button class="bg-button hover:bg-button-hover text-white font-bold py-2 px-4 rounded-full ring-2 ring-button-hover" @click="randomize">
         Randomize
       </button>
     </div>
@@ -22,11 +35,12 @@ export default {
   data() {
     return {
       waifuImg: '',
+      type: 'waifu'
     }
   },
   methods: {
     async randomize() {
-      const response = await fetch("https://api.waifu.pics/sfw/waifu");
+      const response = await fetch(`https://api.waifu.pics/sfw/${this.type}`);
       const data = await response.json();
       this.totalVuePackages = data.total;
       this.waifuImg = data.url;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pink': '#ffcbdb ',
+        'font': '#c6b0d5',
+        'button': '#c6b0d5',
+        'button-hover': '#ad9bba',
+      }
+    },
   },
   variants: {
     extend: {},
